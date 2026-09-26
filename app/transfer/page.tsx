bcfb9915666c5395b26d31c0cf415e08f50097ed
ensite999-stack <ensite999@gmail.com>
2026-09-26 21:13:06 +0800

    Remove unused transfer link import

--- app/transfer/page.tsx
+++ app/transfer/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import {FormEvent,useEffect,useState} from "react";
-import Link from "next/link";
-import {useI18n} from "@/components/I18nProvider";
+ import {useI18n} from "@/components/I18nProvider";
 import BackLink from "@/components/BackLink";
 
 export default function Transfer(){
