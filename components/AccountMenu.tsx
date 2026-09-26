4b34e7d85c5dc2d3582cd46240deaa692fccc903
ensite999-stack <ensite999@gmail.com>
2026-09-26 19:42:14 +0800

    Clean up account menu import

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import {usePathname} from "next/navigation";
-import {ReactNode,useEffect,useRef,useState} from "react";
+import {useEffect,useRef,useState} from "react";
 import {useI18n} from "./I18nProvider";
 
 function MenuIcon(){
